b85abac125df65c219828cb5c507061b964aa78e
Dhruv Madhu <dhruvmadhu2@gmail.com>
2026-07-31 21:58:07 +0530

    Update multiplication_table_generator.py

--- multiplication_table_generator.py
+++ multiplication_table_generator.py
@@ -3,7 +3,7 @@ def generateTable(n):
     for i in range (1, 11):
         table += f"{n} * {i} = {n*i}\n"
 
-    with open(f"tables/table_{n}", "w") as f:
+    with open(f"tables/table_{n}.txt", "w") as f:
         f.write(table)
 
 
